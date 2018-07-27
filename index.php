927353109d32fc8d07eee9ad4b30126eb356c9f7
sj_be <sj_be@STEVENOVATECH>
2018-07-27 09:52:31 +0100

    Initial development

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+  <link href="css/styles.css" rel="stylesheet" type="text/css" />
+ </head>
+ <body>
+ <img src="img/logo.png" />
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
